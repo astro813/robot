3e258c88aac84a7bdcecf8faad96e479ea324b62
lizigong <597107401@qq.com>
2015-11-30 15:08:48 -0500

    sensors

--- sensors.cc
+++ sensors.cc
@@ -2,44 +2,47 @@
 #include <cmath>
 using namespace std;
 #include <random>
+#include "robot.cc"
 //need head
 
-class sensors{
+class sensors: public Robot{
 private:
-     int x;
-     int y;
-     int z;
+     int sensors_x;
+     int sensors_y;
+     int sensors_z;
      int t;
-     int velocity;
-     int theta;
+     int sensors_velocity;
+     int sensors_theta;
 public:
-     sensors(int x=0,int y=0,int z=0,int velocity=0,int theta=0):x(x),y(y),z(z),velocity(velocity),theta(theta){}
+     sensors(int x=0,int y=0,int z=0,int velocity=0,int theta=0){
+     Robot(x,y,z,velocity,theta);
+     }
      double normaldistribution(){
      default_random_engine generator;
      normal_distribution<double> distribution(0.0,1.0);
      return distribution(generator);
      }
      int radar(){
-     this->z= this->z+normaldistribution();
+     this->sensors_z= get_z()+normaldistribution();
      }
      int gps(){
-     this->x= this->x+normaldistribution();
-     this->y= this->y+normaldistribution();
+     this->sensors_x= get_x()+normaldistribution();
+     this->sensors_y= get_y()+normaldistribution();
      }
      int SONAR(){
-     this->z= this->z+normaldistribution();
+     this->sensors_z=get_z()+normaldistribution();
      }
      int compass(){
-     this->theta= this->theta+normaldistribution();
+     this->sensors_theta= this->theta+normaldistribution();
      }
      int accelerometer(){
-     this->velocity= this->velocity+normaldistribution();
+     this->sensors_velocity= this->velocity+normaldistribution();
      }
      int Altimeter(){
-     this->z= this->z+normaldistribution();
+     this->sensors_z= get_z()+normaldistribution();
      }
      friend ostream& operator <<(ostream& s, sensors c){
-     return s << c.x<<" " <<c.y<< " "<< c.z<<" " <<c.velocity<<" " <<c.theta<<" " ;
+     return s << c.get_x()<<" " <<c.get_y()<< " "<< c.get_z()<<" " <<c.velocity<<" " <<c.theta<<" " ;
      }
 };
 
