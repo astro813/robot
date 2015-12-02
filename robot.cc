6868148338827ab791d86bb43ab77eef7f64f3fa
lizigong <597107401@qq.com>
2015-12-01 21:38:22 -0500

    1

--- robot.cc
+++ robot.cc
@@ -6,7 +6,6 @@ using namespace std;
 
 class Robot{
 private:
-
      double x;
      double y;
      double z;
@@ -15,45 +14,45 @@ private:
      vector<double> realVelocity;
      vector<double> believeVelocity;
 protected:
-    int velocity;
-    int theta;
+    double velocity;
+    double theta;
 public:
-    Robot(int x=0,int y=0,int z=0, int velocity=0, int theta=0):x(x),y(y),z(z),velocity(velocity),theta(theta){}
+    Robot(double x=0,double y=0,double z=0, double velocity=0, double theta=0):x(x),y(y),z(z),velocity(velocity),theta(theta){}
 
-    void setlocation(int x, int y, int z=0){this->x=x;this->y=y;this->z=z;}
+    void setlocation(double x, double y, double z=0){this->x=x;this->y=y;this->z=z;}
 
-    virtual int get_x(){
+    virtual double get_x(){
      return this->x;
     }
 
-    virtual int get_y(){
+    virtual double get_y(){
      return this->y;
     }
 
-    virtual int get_z(){
+    virtual double get_z(){
      return this->z;
     }
 
-    virtual void set_x(int input_x){
+    virtual void set_x(double input_x){
      this->x=input_x;
     }
 
-    virtual void set_y(int input_y){
+    virtual void set_y(double input_y){
      this->y=input_y;
     }
 
-    virtual void set_z(int input_z){
+    virtual void set_z(double input_z){
      this->z=input_z;
     }
 
-    void updatelocation(int v, int theta)
+    void updatelocation(double v, double theta)
     {
         this->x=this->x+ velocity*cos(theta);
         this->y=this->y+ velocity*sin(theta);
     }
 
-      int* getlocation(){
-        int location[3] ;
+      double* getlocation(){
+        double location[3] ;
         location[0]=this->x;
         location[1]=this->y;
         location[2]=this->z;
@@ -69,7 +68,7 @@ public:
 int main(){
 Robot car(2.0,1.0,0);
 car.setlocation(1.0,2.0,0.0);
-int *location=car.getlocation();
+double *location=car.getlocation();
 cout<< "x="<<location[0]<<" "<<"y="<<location[1]<<" "<<"z="<<location[2];
 return 0;
 }
