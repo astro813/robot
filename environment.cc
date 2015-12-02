7133ae4f77246edbe917b2a324ab63f52f5c332a
lizigong <597107401@qq.com>
2015-12-01 20:26:47 -0500

    rebuild, need txt

--- environment.cc
+++ environment.cc
@@ -1,84 +1,48 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 using namespace std;
 
 class environment{
 private:
-       int land_length;// should be double (size of world in x and y)
-       int land_width;
-       int ocean_length;
-       int ocean_width;
-       int ocean_depth;
-       int air_height;
-       int ***envir;
-       // need a 2d or 3d grid of land, water, (and if 3d, air)
+    double HEIGHT;
+    double WIDTH;
+    double DEPTH;
+    vector<vector<vector<double> > > environment3D;
+    // need a 2d or 3d grid of land, water, (and if 3d, air)
        //environment needs a list of robots...
 public:
-      environment(int land_length,int land_width, int ocean_length,int ocean_width,int ocean_depth,int air_height):land_length(land_length),
-      land_width(land_width),ocean_length(ocean_length),ocean_width(ocean_width),ocean_depth(ocean_depth),air_height(air_height){
-        int ***envir = new int ** [land_length+ocean_length];
-          for (int i = 0;i < land_length+ocean_length;i++)
-            {
-                envir[i] = new int*[land_width+ocean_width];
-          for (int j = 0;j < land_width+ocean_width;j++)
-            {
-                envir[i][j] = new int [ocean_depth+air_height];
-          }
-          }
-
-          for (int k = 0;k < land_length+ocean_length;k++)
-            {
-                for (int j = 0;j < land_width+ocean_width;j++)
+      environment(double HEIGHT,double WIDTH,double DEPTH):HEIGHT(HEIGHT),WIDTH(WIDTH),DEPTH(DEPTH)
+      {
+       environment3D.resize(HEIGHT);
+      for (int i = 0; i < HEIGHT; i++)
+        {
+            environment3D[i].resize(WIDTH);
+            for (int j = 0; j < WIDTH; j++)
                 {
-                    for (int i = 0;i < ocean_depth+air_height;i++)
-                    {
-                        envir[k][j][i] = 0;
-          }
-          }
-          }
-
-          for (int k = 0;k < land_length;k++)
-            {
-                for (int j = 0;j < land_width;j++)
-                {
-
-                        envir[k][j][ocean_depth] = 1;
-          }
-          }
-           for (int k = land_length;k < land_length+ocean_length;k++)
-            {
-                for (int j = land_width;j < land_width+ocean_width;j++)
-                {
-
-                        envir[k][j][ocean_depth] = 2;
-          }
-          }
-         for (int k = land_length;k < land_length+ocean_length;k++)
-            {
-                for (int j = land_width;j < land_width+ocean_width;j++)
-                {
-                    for (int i = 0;i < ocean_depth;i++)
-                    {
-                        envir[k][j][i] = 3;
-          }
-          }
-          }
-           for (int k = 0;k < land_length+ocean_length;k++)
-            {
-                for (int j = 0;j < land_width+ocean_width;j++)
-                {
-                    for (int i = ocean_depth;i < ocean_depth+air_height;i++)
-                    {
-                        envir[k][j][i] = 4;
-          }
-          }
-          }
+                    environment3D[i][j].resize(DEPTH);
+            }
+        }
       }
 
+      int getdwidth(){
+      return this->WIDTH;
+      }
       int getdepth(){
-      return this->ocean_depth;
+      return this->DEPTH;
       }
       int getheight(){
-      return this->air_height;
+      return this->HEIGHT;
+      }
+      int getenvironment(double HEIGHT,double WIDTH,double DEPTH)
+      {
+          return environment3D[HEIGHT][WIDTH][DEPTH];
       }
 };
+
+/*
+int main(){
+environment c(1,2,2);
+cout<<c.getheight();
+}
+*/
